c636c99823c0158bfc5471c724a0b8507785e135
Henry Garcia <hgarcia8889@gmail.com>
2025-07-10 19:44:19 -0400

    Formatted documents.

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -24,7 +24,7 @@ function PokeCard() {
         if (Number(id) <= 649) {
             return 5;
         }
-        
+
         return 6;
 
     }
@@ -32,11 +32,11 @@ function PokeCard() {
     const audioRef = useRef();
 
     const [data, setData] = useState(
-        {imgSrc: null, monName: '', genus: '', typeNames: [], hieght: '', weight: '', description: '', latest: null}
+        { imgSrc: null, monName: '', genus: '', typeNames: [], hieght: '', weight: '', description: '', latest: null }
     );
 
     const URLs = [`https://pokeapi.co/api/v2/pokemon/${id}`, `https://pokeapi.co/api/v2/pokemon-species/${id}`]
-    
+
     const getMon = async () => {
         try {
             const monData1 = await axios.get(URLs[0]);
@@ -44,15 +44,15 @@ function PokeCard() {
 
             const imgSrc = monData1.data.sprites.other['official-artwork'].front_default;
             const monName = monData2.data.name.charAt(0).toLocaleUpperCase() + monData2.data.name.slice(1);
-            const {genus} = monData2.data.genera.find((e) => {return e.language.name === "en"});
-            const {types}= monData1.data;
-            const typeNames = types.map(e => {return e.type.name})
-            const description = monData2.data.flavor_text_entries.find((e) => {return e.language.name === "en" && e.version.name === 'omega-ruby'}).flavor_text;
-            const {latest} = monData1.data.cries;
+            const { genus } = monData2.data.genera.find((e) => { return e.language.name === "en" });
+            const { types } = monData1.data;
+            const typeNames = types.map(e => { return e.type.name })
+            const description = monData2.data.flavor_text_entries.find((e) => { return e.language.name === "en" && e.version.name === 'omega-ruby' }).flavor_text;
+            const { latest } = monData1.data.cries;
 
             console.log(latest);
 
-            setData({imgSrc, monName, genus, typeNames, description, latest});
+            setData({ imgSrc, monName, genus, typeNames, description, latest });
 
         } catch (error) {
 
@@ -62,41 +62,41 @@ function PokeCard() {
     }
 
     const handleCry = () => {
-        
+
         audioRef.current.volume = 0.12;
         audioRef.current.play()
 
     }
 
-    useEffect(() => { 
+    useEffect(() => {
         getMon();
         audioRef.current.volume = 0.12;
         audioRef.current.play()
-     }, [id]);
+    }, [id]);
 
     const types = data.typeNames.map(element => {
-        return <div key={data.typeNames.indexOf(element) + 1 } className="type" id={element}>{element.charAt(0).toLocaleUpperCase() + element.slice(1)}</div>
+        return <div key={data.typeNames.indexOf(element) + 1} className="type" id={element}>{element.charAt(0).toLocaleUpperCase() + element.slice(1)}</div>
     })
 
-    return(
+    return (
         <>
-        <div className="wrapper">
-            { id !== '1' ? <Link to={`/pokemon/${Number(id) - 1}`}><p className="card-container">← Previous</p></Link> : null}
-        <div className="card-container">
-            <img className="card-img" src={data.imgSrc} alt={data.monName} onClick={handleCry}/>
-            <div className="card-content">
-                <p className="index">{'# ' + id}</p>
-            <h1>{data.monName}</h1>
-            <h3>{data.genus}</h3>
-            <div className="type-container">{types}</div>
-            <div>{data.description}</div>
+            <div className="wrapper">
+                {id !== '1' ? <Link to={`/pokemon/${Number(id) - 1}`}><p className="card-container">← Previous</p></Link> : null}
+                <div className="card-container">
+                    <img className="card-img" src={data.imgSrc} alt={data.monName} onClick={handleCry} />
+                    <div className="card-content">
+                        <p className="index">{'# ' + id}</p>
+                        <h1>{data.monName}</h1>
+                        <h3>{data.genus}</h3>
+                        <div className="type-container">{types}</div>
+                        <div>{data.description}</div>
+                    </div>
+                    <audio ref={audioRef} src={data.latest} />
+                </div>
+                {id !== '721' ? <Link to={`/pokemon/${Number(id) + 1}`}><p className="card-container">Next →</p></Link> : null}
             </div>
-            <audio ref={audioRef} src={data.latest} />
-        </div>
-            { id !== '721' ? <Link to={`/pokemon/${Number(id) + 1}`}><p className="card-container">Next →</p></Link> : null}
-        </div>
-        <Link to={`/pokelist/${regionId(id)}`}><div>Go Back to Region's Pokémon List</div></Link>
-        <Link to={`/pokelist/0`}><div>Go Back to All Pokémon List</div></Link>
+            <Link to={`/pokelist/${regionId(id)}`}><div>Go Back to Region's Pokémon List</div></Link>
+            <Link to={`/pokelist/0`}><div>Go Back to All Pokémon List</div></Link>
         </>
     )
 }
--- src/components/PokeListItem.jsx
+++ src/components/PokeListItem.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-function PokeListItem({ id, name }){
+function PokeListItem({ id, name }) {
 
-    let i = id < 100 ? ( id < 10 ? '00' + id : '0' + id ) : id;
-    
-    return(
+    let i = id < 100 ? (id < 10 ? '00' + id : '0' + id) : id;
+
+    return (
         <Link to={`/pokemon/${id}`}>
             <div className="list-item"><span className="index">{i} </span> {name.charAt(0).toLocaleUpperCase() + name.slice(1)}</div>
         </Link>
