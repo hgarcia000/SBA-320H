eb403f9538add59e81921e454723527f404a8ab0
Henry Garcia <hgarcia8889@gmail.com>
2025-07-09 20:24:28 -0400

    Updated region array logic.

--- src/components/RegionList.jsx
+++ src/components/RegionList.jsx
@@ -1,53 +1,19 @@
+import region from "../Region.js";
 import RegionListItem from "./RegionListItem";
 
 
 function RegionList() {
 
-    const regionData = [{
-                        name:'Kanto',
-                        range: '001 - 151',
-                        limit: 151,
-                        offset: 0
-                    },
-                    {
-                        name:'Johto',
-                        range: '152 - 251',
-                        limit: 100,
-                        offset: 151
-                    },
-                    {
-                        name:'Hoenn',
-                        range: '252 - 386',
-                        limit: 135,
-                        offset: 251
-                    },
-                    {
-                        name:'Sinnoh',
-                        range: '387 - 493',
-                        limit: 107,
-                        offset: 386
-                    },
-                    {
-                        name:'Unova',
-                        range: '494 - 649',
-                        limit: 156,
-                        offset: 493
-                    },
-                    {
-                        name:'Kalos',
-                        range: '650 - 721',
-                        limit: 72,
-                        offset: 649
-                    }];
+    const regionIndecies = ['1', '2', '3', '4', '5', '6'];
 
-                    const regions = regionData.map(element => {
-                        return <RegionListItem key={regionData.indexOf(element) + 1} id={regionData.indexOf(element) + 1} name={element.name} range={element.range} limit={element.limit} offset={element.offset}/>
-                    })
+    const regionListItems = regionIndecies.map(element => {
+        return <RegionListItem key={regionIndecies.indexOf(element) + 1} id={element} name={region(element).name} range={region(element).range} />
+    })
 
     return (
         <>
             <h1>Select a Region</h1>
-            {regions}
+            {regionListItems}
         </>
     )
 }
