6fcf6277f2393bc20ece79a9470916f4780bff76
Henry Garcia <hgarcia8889@gmail.com>
2025-07-09 21:32:50 -0400

    Added Card component template and guidelines.

---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,17 @@
+
+
+function PokeCard(params) {
+    
+    return(
+        <div>
+            <div>Image</div>
+            <div>Title</div>
+            <div>Genus</div>
+            <div>Types</div>
+            <div>Flavor Text</div>
+            <div>Battle Cry</div>
+        </div>
+    )
+}
+
+export default PokeCard
